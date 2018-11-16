39d99ef055e3c72170dd8d423c7ce97d19928cd0
Color-cloud <1243488616@qq.com>
2018-11-16 22:55:22 +0800

    little change       ----come from wng

--- APPS/Account/api.py
+++ APPS/Account/api.py
@@ -2,26 +2,25 @@ from flask_restful import Resource, fields, marshal_with
 
 # 数据json数据格式
 # 第一步定义数据格式
-from APPS.Account.models import User
+from APPS.Account.models import User, UserNext
 
-stu_fields = {
-    's_id': fields.Integer,
-    's_age': fields.Integer,
-    's_name': fields.String,
+user_fields = {
+    'u_id': fields.Integer,
+    'username': fields.String,
 }
-cou_fields = {
-    's_id_id': fields.Integer,
-    'c_name': fields.String,
+usernext_fields = {
+    'n_uid': fields.Integer,
+    'n_username': fields.String,
 }
 wwww = {
-    'stus': fields.List(fields.Nested(stu_fields)),
-    'cous': fields.List(fields.Nested(cou_fields)),
+    'stus': fields.List(fields.Nested(user_fields)),
+    'cous': fields.List(fields.Nested(usernext_fields)),
 }
 result = {
     'status': fields.Integer(),
     'msg': fields.String,
     # "data": fields.List(fields.Nested(wwww)),
-    'data':fields.List(fields.Nested(cou_fields)),
+    'data': fields.List(fields.Nested(user_fields)),
 }
 
 # 装饰 marshal_with
@@ -38,7 +37,6 @@ result = {
 class IndexApi(Resource):
     @marshal_with(result)
     def get(self):
-        cous = User.query.filter(User.u_id==1).first()
-        qw = cous.students
-        print(123)
+        cous = UserNext.query.filter(UserNext.n_uid == 1).first()
+        qw = cous.user
         return {'status': 200, 'msg': 'success', 'data': qw}
